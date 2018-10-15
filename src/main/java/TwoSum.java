c0f8ab44e54385391d275329067b4573a6a2894b
lin <linjpg@163.com>
2018-10-15 23:46:38 +0800

    'first-commit'

---
+++ src/main/java/TwoSum.java
@@ -0,0 +1,56 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by lin on 2018/10/14.
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        int target = 5;
+        int a[] = {};
+        a = TwoSum.twoSum2(nums,target);
+        for(int i = 0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+
+    public static int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    System.out.println(new int[] { i, j });
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static int[] twoSum3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
